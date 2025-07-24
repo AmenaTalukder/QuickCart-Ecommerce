553c3b74bdf2d2e241b4dd8ea0406c88fe9096f4
AmenaTalukder <amenaakter9714@gmail.com>
2025-07-24 11:26:44 +0600

    all new

--- config/inngest.js
+++ config/inngest.js
@@ -27,3 +27,41 @@ export const syncUserCreation = inngest.createFunction(
     await User.create(userData);
   }
 );
+
+// Inngest Function to update user data in database
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: "update-user-from-clerk"
+  },
+  {
+    event: "clerk/user.updated"
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      imagUrl: image_url
+    };
+    await connectDB();
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+// Inngest Function to delete user data from database
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-from-clerk"
+  },
+  {
+    event: "clerk/user.deleted"
+  },
+  async ({ event }) => {
+    const { id } = event.data;
+    await connectDB();
+    await User.findByIdAndDelete(id);
+  }
+);
